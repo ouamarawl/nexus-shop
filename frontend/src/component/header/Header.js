4d6d8aea12071602b82d7873d7fcbfc577af5af6
mohamedamine019 <haicheurmohamedamine>
2025-02-16 14:20:17 +0100

    ana hwa babak ya wail²

--- frontend/src/component/header/Header.js
+++ frontend/src/component/header/Header.js
@@ -1,38 +1,82 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import nexus from '../Assets/nexus.jpg';
-import './Header.css';
-import Searchbar from '../Searchbar/Searchbar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faAddressCard, faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons'; 
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import nexus from "../Assets/nexus.jpg";
+import Searchbar from "../Searchbar/Searchbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes, faHouse, faAddressCard, faUser, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import "./Header.css";
 
 function Header() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className='navbar'> 
-      <img src={nexus} alt='nexus' />
-      <div className='list-navbar' style={{ textDecoration: 'none' }}>
-        <Link to='/'>
+    <div className="navbar">
+      <p className="logo">
+        Nexus <img src={nexus} alt="nexus" /> Shop
+      </p>
+
+      <div className="list-navbar">
+        <Link to="/">
           <li>
-            <FontAwesomeIcon icon={faHouse} style={{ color: "#FFD43B" }} /> home
+            <FontAwesomeIcon icon={faHouse} /> Home
           </li>
         </Link>
-        <Link to='/about'>
+        <Link to="/about">
           <li>
-            <FontAwesomeIcon icon={faAddressCard} style={{ color: "#FFD43B" }} /> About
+            <FontAwesomeIcon icon={faAddressCard} /> About
           </li>
         </Link>
-        <Link to='/admin'>
+        <Link to="/admin">
           <li>
-            <FontAwesomeIcon icon={faUser} style={{ color: "#FFD43B" }} /> Admin
+            <FontAwesomeIcon icon={faUser} /> Admin
           </li>
         </Link>
-        <Link to='/panier'>
+        <Link to="/panier">
           <li>
-            <FontAwesomeIcon icon={faCartShopping} style={{ color: "#FFD43B" }} /> Basket
+            <FontAwesomeIcon icon={faCartShopping} /> Basket
           </li>
         </Link>
       </div>
+
       <Searchbar />
+
+      <div className="sidebar-toggle" onClick={() => setSidebarOpen(true)}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+
+      <div className={`sidebar ${sidebarOpen ? "open" : ""}`}>
+        <div className="sidebar-close" onClick={() => setSidebarOpen(false)}>
+          <FontAwesomeIcon icon={faTimes} />
+        </div>
+
+        {/* Searchbar inside Sidebar */}
+        <div className="sidebar-search">
+          <Searchbar />
+        </div>
+
+        <ul>
+          <li>
+            <Link to="/" onClick={() => setSidebarOpen(false)}>
+              <FontAwesomeIcon icon={faHouse} /> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={() => setSidebarOpen(false)}>
+              <FontAwesomeIcon icon={faAddressCard} /> About
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin" onClick={() => setSidebarOpen(false)}>
+              <FontAwesomeIcon icon={faUser} /> Admin
+            </Link>
+          </li>
+          <li>
+            <Link to="/panier" onClick={() => setSidebarOpen(false)}>
+              <FontAwesomeIcon icon={faCartShopping} /> Basket
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
